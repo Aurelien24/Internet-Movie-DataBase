2c24b713ab0cf04378e84048b6357b2f4a2403ea
Aurelien24 <73949657+Aurelien24@users.noreply.github.com>
2023-04-17 17:30:13 +0200

    pré code

--- src/main/java/dev/AppMethode.java
+++ src/main/java/dev/AppMethode.java
@@ -1,5 +1,7 @@
 package dev;
 
+import dev.domain.Acteurs;
+import dev.domain.Langues;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
@@ -8,11 +10,12 @@ import jakarta.persistence.Persistence;
 public class AppMethode {
     public void cree_langue(String langue){
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        System.out.println("Entity Manager Factory Fait !");
         EntityManager em = entityManagerFactory.createEntityManager();
 
         //code
-        Langue langueObjet = new Langue();
-        langueObjet.setLangue(langue);
+        Langues langueObjet = new Langues();
+        langueObjet.setLibL(langue);
 
         EntityTransaction et = em.getTransaction();
         et.begin();
@@ -21,24 +24,72 @@ public class AppMethode {
         em.close();
     }
 
-    public void filmographieActeur(){
+    public void filmographieActeur(String prenom, String nom){
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        EntityManager em = entityManagerFactory.createEntityManager();
 
+        // CODE
+        em.find();
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        //em.persist(langueObjet);
+        et.commit();
+        em.close();
     }
 
-    public void castingFilm(){
+    public void castingFilm(String filme){
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        EntityManager em = entityManagerFactory.createEntityManager();
 
+        // CODE
+        //em.find(Films.class, ID) -> Comment le faire avec le nom et non avec l'id? Comment obtenir l'id ?
+        // Prendre tout les
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        //em.persist(langueObjet);
+        et.commit();
+        em.close();
     }
 
-    public void filmsSortisEntre(){
+    public void filmsSortisEntre(int année1, int année2){
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        EntityManager em = entityManagerFactory.createEntityManager();
 
+        // CODE
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        //em.persist(langueObjet);
+        et.commit();
+        em.close();
     }
 
-    public void  filmsCommuns(){
+    public void  filmsCommuns(String acteur1Prenom, String acteur1Nom, String acteur2Prenom, String acteur2Nom){
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        EntityManager em = entityManagerFactory.createEntityManager();
 
+        // CODE
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        //em.persist(langueObjet);
+        et.commit();
+        em.close();
     }
 
-    public void acteursCommuns(){
+    public void acteursCommuns(String filmes1, String filmes2){
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        EntityManager em = entityManagerFactory.createEntityManager();
 
+        // CODE
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        //em.persist(langueObjet);
+        et.commit();
+        em.close();
     }
 
     // Le point 6 Affichage des films sortis entre 2 années données et qui ont un acteur/actrice donné au
