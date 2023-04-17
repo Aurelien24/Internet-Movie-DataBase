91a387b1988d2b23e70f209946829886a79a107e
Aurelien24 <73949657+Aurelien24@users.noreply.github.com>
2023-04-17 15:47:27 +0200

    bug connect

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,10 +1,41 @@
 package dev;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ecrire le nom de la langue : ");
+        String langue = scanner.nextLine();
+        System.out.println("Envoit de la requete de création de langue...");
 
-    }
+        AppMethode appMethode = new AppMethode();
+        appMethode.cree_langue(langue);
 
-    public static class AppMethode {
+
+        // L'utilisation de whil pourrais etre une bonne idée
+        System.out.println("Voici le menu, taper 1 pour Afficher la filmographie d’un acteur donné, taper 2 pour afficher le casting d’un film, 3 pour afficher des films sortis entre 2 années données, 4 pour afficher des films communs à 2 acteurs/actrices donnés, 5 pour afficher des acteurs communs à 2 films donnés, 6 pour afficher des films sortis entre 2 années données et qui ont un acteur/actrice donné, 7 pour annuler.");
+        int choix = scanner.nextInt();
+        // equals ne prend pas les int
+        if (choix == 1){
+            appMethode.filmographieActeur();
+        } else if (choix == 2) {
+            appMethode.castingFilm();
+        } else if (choix == 3) {
+            appMethode.filmsSortisEntre();
+        } else if (choix == 4) {
+            appMethode.filmsCommuns();
+        } else if (choix == 5) {
+            appMethode.acteursCommuns();
+        } else if (choix == 6) {
+            appMethode.filmsCommuns();
+            appMethode.acteursCommuns();
+        } else if (choix == 7) {
+            System.out.println("L'application va se fermé. Toute données non sauvegardé seras supprimé.");
+            System.out.println("Fermeture de l'application.");
+        } else {
+            System.out.println("Le système n'as pas compris votre demande.");
+            System.out.println("Fermeture de l'application.");
+        }
     }
 }
--- src/main/java/dev/AppMethode.java
+++ src/main/java/dev/AppMethode.java
@@ -1,21 +1,48 @@
 package dev;
 
 import dev.domain.Adresse;
+import dev.domain.Langue;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class AppMethode {
-    public void cree_client(String nom, String prenom, int numero, String rue, int codePostal, String ville){
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("formation-pu");
+    public void cree_langue(String langue){
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
         EntityManager em = entityManagerFactory.createEntityManager();
 
         //code
+        Langue langueObjet = new Langue();
+        langueObjet.setLangue(langue);
 
         EntityTransaction et = em.getTransaction();
         et.begin();
+        em.persist(langueObjet);
         et.commit();
         em.close();
     }
+
+    public void filmographieActeur(){
+
+    }
+
+    public void castingFilm(){
+
+    }
+
+    public void filmsSortisEntre(){
+
+    }
+
+    public void  filmsCommuns(){
+
+    }
+
+    public void acteursCommuns(){
+
+    }
+
+    // Le point 6 Affichage des films sortis entre 2 années données et qui ont un acteur/actrice donné au
+    //casting regroupe le 4 et le 5
 }
