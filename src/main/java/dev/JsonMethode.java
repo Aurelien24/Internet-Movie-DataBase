036d20eafa6353c32e902a2ac6bc012b3f4c40c9
Florian ARNAU <aranauflorian@outlook.com>
2023-04-19 13:58:00 +0200

    jsonMethode v1

--- src/main/java/dev/JsonMethode.java
+++ src/main/java/dev/JsonMethode.java
@@ -1,4 +1,138 @@
 package dev;
 
+import dev.domain.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.Date;
+
 public class JsonMethode {
+    public void addFilm(Integer id, String nom, String annee, String url, String desc){
+        System.out.println("Ajout de film en cours !");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        System.out.println("Entity Manager Factory Fait !");
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        Film film = new Film();
+        film.setId(id);
+        film.setNom(nom);
+        film.setAnnee(annee);
+        film.setUrl(url);
+        film.setDesc(desc);
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(film);
+        et.commit();
+        em.close();
+    }
+
+    public void addLangue(Integer id, String libelle){
+        System.out.println("Ajout de langue !");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        System.out.println("Entity Manager Factory Fait !");
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        Langue langue = new Langue();
+        langue.setId(id);
+        langue.setLibL(libelle);
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(langue);
+        et.commit();
+        em.close();
+    }
+
+    public void addGenre(Integer id, String lib){
+        System.out.println("Ajout de Genre !");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        System.out.println("Entity Manager Factory Fait !");
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        Genre genre = new Genre();
+        genre.setId(id);
+        genre.setLib(lib);
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(genre);
+        et.commit();
+        em.close();
+    }
+
+    public void addAdresse(Integer id, String etat, String pays, String ville){
+        System.out.println("Ajout d'adresse !");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        System.out.println("Entity Manager Factory Fait !");
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        Adresse adresse = new Adresse();
+        adresse.setId(id);
+        adresse.setEtat(etat);
+        adresse.setPays(pays);
+        adresse.setVille(ville);
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(adresse);
+        et.commit();
+        em.close();
+    }
+
+    public void addPersonne(Integer id, String nom, String prenom, String url){
+        System.out.println("Ajout d'une personne !");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        System.out.println("Entity Manager Factory Fait !");
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        Personne personne = new Personne();
+        personne.setId(id);
+        personne.setNom(nom);
+        personne.setPrenom(prenom);
+        personne.setUrl(url);
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(personne);
+        et.commit();
+        em.close();
+    }
+
+    public void addActeur(Personne personne, Date dateNaiss, Adresse adresse){
+        System.out.println("Ajout d'un acteur !");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        System.out.println("Entity Manager Factory Fait !");
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        Acteur acteur = new Acteur();
+        acteur.setDateNaiss(dateNaiss);
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(acteur);
+        et.commit();
+        em.close();
+    }
+
+    public void addNation(Integer id, String libelle, String url){
+        System.out.println("Ajout d'une nationnalité !");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("mainPersistence");
+        System.out.println("Entity Manager Factory Fait !");
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        Nationnalite nationnalite = new Nationnalite();
+        nationnalite.setId(id);
+        nationnalite.setLibN(libelle);
+        nationnalite.setUrlN(url);
+
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+        em.persist(nationnalite);
+        et.commit();
+        em.close();
+    }
+
 }
